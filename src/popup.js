cd98176e2d8f8d7f1332cbf753599fb0a182c076
yanka deshkovski <yankadeshkovski@ufl.edu>
2026-03-25 18:42:49 -0400

    working on algo choices

--- src/popup.js
+++ src/popup.js
@@ -1,21 +1,42 @@
 // Since we are going to use only one logic JS script
 // make sure to use if (exists) for buttons and other things so we don't get errors
 
+let currentOrganization = 0;
+let algorithmChoice = Math.floor(Math.random() * 6) + 1;
+
+const algoDisplay = document.getElementById('algoText');
+
+if (currentOrganization === 0) {
+  algoDisplay.textContent = "Unprioritized";
+} else if (currentOrganization === 1) {
+  algoDisplay.textContent = "Earliest Deadline";
+} else if (currentOrganization === 2) {
+  algoDisplay.textContent = "Easiest Difficulty";
+} else if (currentOrganization === 3) {
+  algoDisplay.textContent = "Hardest Difficulty";
+} else if (currentOrganization === 4) {
+  algoDisplay.textContent = "Intermittent Times";
+} else if (currentOrganization === 5) {
+  algoDisplay.textContent = "Random";
+}
+
+
+
 class Task {
   constructor(_title, _date, _time, _difficulty) {
-    this.title = _title
-    this.date = _date
-    this.time = _time
-    this.difficulty = _difficulty
-    this.status_completed = false
+    this.title = _title;
+    this.date = _date; //    ->    MM/DD/YYYY
+    this.time = _time;
+    this.difficulty = _difficulty;
+    this.status_completed = false;
   }
 
   mark() {
-    this.status_completed = true
+    this.status_completed = true;
   }
 
   unmark() {
-    this.status_completed = false
+    this.status_completed = false;
   }
 }
 
